f399a473d926052d8fe77ff45e45ac32f408027e
Saeed <hassanisaeed19@yahoo.com>
2023-08-07 21:32:27 +0330

    initial commit

--- middleware/authenticate.js
+++
@@ -1,6 +0,0 @@
-export default ({ store, redirect }) => {
-  if (!store.state.auth.token) {
-    store.dispatch('auth/logoutConfig').then(() => {})
-    redirect('/')
-  }
-}
--- plugins/filters.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-
-import datetime from 'persian-time-ago'
-
-Vue.filter('timeago', function (value) {
-  return datetime(value)
-})
